bf1ec2c698a085ac7db391626a065aa78090e772
Guille Lavado <lavadoguille@gmail.com>
2025-10-29 09:37:09 +0100

    añadir texto a pagina quinesSomo

--- quienesSomos.php
+++ quienesSomos.php
@@ -6,11 +6,21 @@
     <link rel="stylesheet" href="public/css/footer.css">
     <link rel="stylesheet" href="public/css/menu.css">
     <title>Accerca De</title>
+    <link rel="stylesheet" href="public/css/quienesSomos.css">
 </head>
 <body>
     <?php include("views/menu.php"); ?>
     <main>
-        <h1></h1>
+        <section>
+            <h1>¿Por que esta web?</h1>
+            <p>
+                Esta web esta pensanda como una empresa que vende exclusivamente cubos de rubik,
+                el diseño esta creado paras ser minimalista y enfocado en no abarrotar la vista del usuario.<br>
+                El uso de los colores principales de la web viene los seis colores que tiene
+                un cubo 3x3 (blanco, amarillo, rojo, azul, verde y naranja), esto hace a la web identificativa y
+                colorida para calar en la mente del usuario.<br>
+            </p>
+        </section>
     </main>
     <?php include("views/footer.php") ?>
 </body>
